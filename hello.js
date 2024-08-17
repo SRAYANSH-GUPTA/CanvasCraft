c5917a2ef5efc13c8b5226e5ecb55101ff33e5c0
SRAYANSH-GUPTA <144322123+SRAYANSH-GUPTA@users.noreply.github.com>
2024-08-17 09:23:48 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,7 @@
+   const about = document.getElementById("about");
+    console.log(about);
+    const tagname = document.getElementsByTagName("p");
+    console.log(tagname);
+    const selector = document.querySelectorAll("p");
+    console.log(selector);
+
